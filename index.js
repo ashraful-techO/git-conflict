0b88edb58b78e1c78c1a5c0b77e6868800c4834a
ashraful-techO <ashraful.islam.cse.97@gmail.com>
2025-09-01 12:35:47 +0600

    new branch

--- index.js
+++ index.js
@@ -1,5 +1,1 @@
-
-console.log("Hello from main branch update");
-console.log("Hello from feature branch");
-console.log("Hello from feature branch updated"); //comment added
-
+console.log("Hello from dev");
